697f9e2fd6d88df59b6367131ef009e7f228b805
KritsanaDK <kritsana.dk@gmail.com>
2023-08-11 14:42:19 +0700

    1

---
+++ src/components/ImageData.js
@@ -0,0 +1,17 @@
+
+const ImageData = [
+    {
+        title: "A",
+        image: "https://cdn.pixabay.com/photo/2023/07/28/18/23/bird-8155768_1280.jpg"
+    },
+    {
+        title: "B",
+        image: "https://media.istockphoto.com/id/1032674926/th/%E0%B8%A3%E0%B8%B9%E0%B8%9B%E0%B8%96%E0%B9%88%E0%B8%B2%E0%B8%A2/%E0%B9%80%E0%B8%AB%E0%B8%A2%E0%B8%B5%E0%B9%88%E0%B8%A2%E0%B8%A7%E0%B9%80%E0%B8%9E%E0%B9%80%E0%B8%A3%E0%B8%81%E0%B8%A3%E0%B8%B4%E0%B8%99.jpg?s=2048x2048&w=is&k=20&c=ksCtDde20ucLPYKOU0nvMQ7F6ayqvdFRHQU7hdBfSEA="
+    }, {
+        title: "C",
+        image: "https://media.istockphoto.com/id/177257555/th/%E0%B8%A3%E0%B8%B9%E0%B8%9B%E0%B8%96%E0%B9%88%E0%B8%B2%E0%B8%A2/%E0%B9%81%E0%B8%A5%E0%B8%99%E0%B9%80%E0%B8%99%E0%B8%AD%E0%B8%A3%E0%B9%8C%E0%B8%9F%E0%B8%AD%E0%B8%A5%E0%B8%84%E0%B8%AD%E0%B8%99%E0%B8%A5%E0%B8%87%E0%B8%88%E0%B8%AD%E0%B8%94.jpg?s=2048x2048&w=is&k=20&c=0XhBIs1KA89JPxZdVP0y6LBLsAeZ4tyg5HwgG18L4jE="
+    }
+
+];
+
+export default ImageData;
